27149b2ddd43b89a64652564fdfddf294d356dac
vvoskanyan <vvoskanyan@vmware.com>
2018-11-24 22:40:38 +0400

    added exceptions for some endpoints

--- src/main/java/com/ysu/xrandnet/config/SecurityConfig.java
+++ src/main/java/com/ysu/xrandnet/config/SecurityConfig.java
@@ -77,6 +77,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/",
                         "/favicon.ico",
+                        "/announcements/all",
+                        "/releaseNotes/all",
+                        "/about",
                         "/**/*.png",
                         "/**/*.gif",
                         "/**/*.svg",
--- src/main/java/com/ysu/xrandnet/controllers/DataProviderController.java
+++ src/main/java/com/ysu/xrandnet/controllers/DataProviderController.java
@@ -10,6 +10,7 @@ import com.ysu.xrandnet.repos.BugRepository;
 import com.ysu.xrandnet.repos.ReleaseNoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
